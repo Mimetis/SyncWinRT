bc12bb4a7e239e7cae2aa839d31cd45ac14c8b04
hyperise <hyperisecom@gmail.com>
2014-07-12 21:51:54 -0700

    Upgrade to 8.1 Windows and Phone

--- SyncClient.WindowsPhone81/Properties/AssemblyInfo.cs
+++ SyncClient.WindowsPhone81/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SyncFramework.Metro.SyncClient")]
+[assembly: AssemblyTitle("Microsoft.Synchronization.ClientServices.WinPhone81")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SyncFramework.Metro.SyncClient")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("Microsoft.Synchronization.ClientServices.WinPhone81")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
